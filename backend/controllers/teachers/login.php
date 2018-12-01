ee54d8c3f13b9303ef5e651d6e16e3e2f5fe8ed6
Pushkar Anand <anandpushkar088@gmail.com>
2018-12-01 12:45:37 +0530

    added routes.

---
+++ backend/controllers/teachers/login.php
@@ -0,0 +1,7 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: pushkar
+ * Date: 1/12/18
+ * Time: 12:44 PM
+ */
--- backend/routes.php
+++ backend/routes.php
@@ -3,4 +3,12 @@ require_once __DIR__ . "/vendor/autoload.php";
 
 $route = new \EasyRoute\Route();
 
-$route->execute();
+try {
+    $route->addMatch('POST', '/teacher/login', __DIR__ . '/controllers/teachers/login.php');
+
+
+    $route->execute();
+
+} catch (Exception $e) {
+}
+
