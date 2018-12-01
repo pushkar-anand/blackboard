c7162bb461d56753c44370edcbf8816e63adb447
Pushkar Anand <anandpushkar088@gmail.com>
2018-12-01 18:28:40 +0530

    dao

---
+++ android/app/src/main/java/io/mlh/localhackday/blackboard/db/StudentDao.kt
@@ -0,0 +1,21 @@
+package io.mlh.localhackday.blackboard.db
+
+import androidx.lifecycle.LiveData
+import androidx.room.Dao
+import androidx.room.Insert
+import androidx.room.Query
+import androidx.room.Update
+import io.mlh.localhackday.blackboard.data.Student
+
+@Dao
+interface StudentDao {
+
+    @Insert
+    fun insert(student: Student)
+
+    @Update
+    fun update(vararg student: Student)
+
+    @Query("SELECT * FROM students WHERE email = :email")
+    fun get(email: String): LiveData<Student>
+}
