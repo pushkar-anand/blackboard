1776d8f4c78c599806e06e783d79b3605c1896b3
Adith A Danthi <adith.danthi@gmail.com>
2018-12-01 11:05:53 +0530

    Launcher Activity

--- android/app/src/main/java/io/mlh/localhackday/blackboard/UI/LauncherActivity.kt
+++ android/app/src/main/java/io/mlh/localhackday/blackboard/UI/LauncherActivity.kt
@@ -2,12 +2,11 @@ package io.mlh.localhackday.blackboard.UI
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import io.mlh.localhackday.blackboard.R
 
-class LoginActivity : AppCompatActivity() {
+class LauncherActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_login)
+        setContentView(R.layout.activity_launcher)
     }
 }
