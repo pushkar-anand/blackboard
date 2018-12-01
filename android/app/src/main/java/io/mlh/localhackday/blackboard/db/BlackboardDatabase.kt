43eeb7263f4780022f424a0367e08153df67e245
Pushkar Anand <anandpushkar088@gmail.com>
2018-12-01 18:38:24 +0530

    added db.

---
+++ android/app/src/main/java/io/mlh/localhackday/blackboard/db/BlackboardDatabase.kt
@@ -0,0 +1,35 @@
+package io.mlh.localhackday.blackboard.db
+
+import android.content.Context
+import androidx.room.Database
+import androidx.room.Room
+import androidx.room.RoomDatabase
+import io.mlh.localhackday.blackboard.data.Student
+
+@Database(entities = [Student::class], version = 1)
+abstract class BlackboardDatabase : RoomDatabase() {
+
+    abstract fun studentDao(): StudentDao
+
+    companion object {
+
+        @Volatile
+        private var INSTANCE: BlackboardDatabase? = null
+
+        internal fun getDatabase(context: Context): BlackboardDatabase? {
+            if (INSTANCE == null) {
+                synchronized(Database::class.java) {
+                    if (INSTANCE == null) {
+                        INSTANCE = Room.databaseBuilder(
+                            context.applicationContext,
+                            BlackboardDatabase::class.java, "blackboard.db"
+                        )
+                            .build()
+                    }
+                }
+            }
+            return INSTANCE
+        }
+
+    }
+}
