d586cbd9386a31073b4c61953316f5291e2b5508
Pushkar Anand <anandpushkar088@gmail.com>
2018-12-01 13:26:49 +0530

    added login for students.

--- backend/controllers/students/login.php
+++ backend/controllers/students/login.php
@@ -1,7 +1,39 @@
 <?php
-/**
- * Created by PhpStorm.
- * User: pushkar
- * Date: 1/12/18
- * Time: 12:58 PM
- */
+
+require_once __DIR__ . "/../../vendor/autoload.php";
+
+use PhpUseful\EasyHeaders;
+use PhpUseful\Functions;
+
+if( isset($_POST['email']) && isset($_POST['password'])) {
+
+    $email = Functions::escapeInput($_POST['email']);
+    $pwd = $_POST['password'];
+
+    $student = new Student($email);
+    $studentDetails = $student->getStudent();
+
+    if($studentDetails !== false && $studentDetails !== null) {
+
+        if(password_verify($pwd, $studentDetails['password'])) {
+
+            $random = Functions::randomString(25);
+            $time = time();
+
+            $student->updateToken($random, $time);
+            $student = new Student($email);
+            $studentDetails = $student->getStudent();
+            unset($studentDetails['password']);
+
+            EasyHeaders::json_header();
+            echo json_encode($studentDetails);
+        }
+
+    } else {
+        EasyHeaders::not_found();
+    }
+
+
+} else {
+    EasyHeaders::bad_request();
+}
