b31503af910b6597e06879544070555b22d4d560
Pushkar Anand <anandpushkar088@gmail.com>
2018-12-01 15:41:34 +0530

    new Routes.

--- backend/routes.php
+++ backend/routes.php
@@ -7,6 +7,8 @@ try {
     $route->addMatch('POST', '/teacher/login', __DIR__ . '/controllers/teachers/login.php');
 
     $route->addMatch('POST', '/student/login', __DIR__ . '/controllers/students/login.php');
+    $route->addMatch('GET', '/student/details/semester', __DIR__ . '/controllers/students/details.semester.php');
+    $route->addMatch('GET', '/student/details/subject', __DIR__ . '/controllers/students/details.subject.php');
 
     $route->execute();
 
