60967579fff786c9f725b9da03f406ec63fa2839
Nikhil <r.nikhil1999@gmail.com>
2018-12-01 13:25:05 +0530

    teachers API

--- android/app/src/main/java/io/mlh/localhackday/blackboard/repository/TeacherRepository.kt
+++ android/app/src/main/java/io/mlh/localhackday/blackboard/repository/TeacherRepository.kt
@@ -1,4 +1,21 @@
 package io.mlh.localhackday.blackboard.repository
 
+import android.provider.ContactsContract
+import io.mlh.localhackday.blackboard.api.TeachersAPI
+import io.mlh.localhackday.blackboard.api.Urls
+import io.mlh.localhackday.blackboard.data.Teacher
+import retrofit2.Call
+import retrofit2.Retrofit
+import retrofit2.converter.moshi.MoshiConverterFactory
+
 class TeacherRepository {
+    private val retroService =
+            Retrofit.Builder()
+                .baseUrl(Urls.API_BASE_URL)
+                .addConverterFactory(MoshiConverterFactory.create())
+                .build()
+                .create(TeachersAPI::class.java)
+    fun getLoginResults(email: String, password: String): Call<Teacher> {
+        return retroService.getLoginResults(email,password)
+    }
 }
